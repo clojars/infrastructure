bcad7f39cad29da7a354cc11da958b82e849f5ae
Toby Crawley <toby@tcrawley.org>
2020-01-31 21:55:51 -0500

    Add s3 bucket for log storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,14 @@ provider "aws" {
   region = "us-east-2"
 }
 
+# stats bucket
+
 resource "aws_s3_bucket" "stats_bucket" {
   bucket = "clojars-stats-production"
   acl = "public-read"
 }
+
+# user for rackspace server
 
 resource "aws_iam_user" "rackspace_server" {
   name = "rackspace-server"
@@ -38,4 +42,68 @@ EOF
 resource "aws_iam_user_policy_attachment" "rackspace_policy_attach" {
   user = aws_iam_user.rackspace_server.name
   policy_arn = aws_iam_policy.rackspace_policy.arn
+}
+
+# fastly logs bucket
+
+resource "aws_s3_bucket" "fastly_logs_bucket" {
+  bucket = "clojars-fastly-logs"
+  acl = "private"
+
+  lifecycle_rule {
+    id      = "delete-old-logs"
+    enabled = true
+
+    expiration {
+      days = "14"
+    }
+
+    noncurrent_version_expiration {
+      days = "14"
+    }
+  }
+}
+
+# fastly logs user
+
+resource "aws_iam_user" "fastly_logs" {
+  name = "fastly-logs"
+}
+
+resource "aws_iam_policy" "fastly_logs_policy" {
+  name        = "fastly-logs-policy"
+  description = "A policy for Fastly to write access logs"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket",
+                "s3:GetObject",
+                "s3:PutObject"
+            ],
+            "Resource": [
+                "${aws_s3_bucket.fastly_logs_bucket.arn}",
+                "${aws_s3_bucket.fastly_logs_bucket.arn}/*"
+            ]
+        },
+        {
+            "Effect": "Deny",
+            "NotAction": "s3:*",
+            "NotResource": [
+                "${aws_s3_bucket.fastly_logs_bucket.arn}",
+                "${aws_s3_bucket.fastly_logs_bucket.arn}/*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "fastly_logging_policy_attach" {
+  user = aws_iam_user.fastly_logs.name
+  policy_arn = aws_iam_policy.fastly_logs_policy.arn
 }
