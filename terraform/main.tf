43ef54655390f40ab75378b5c4d6cb06d7253208
Toby Crawley <toby@tcrawley.org>
2020-03-29 08:59:26 -0400

    Remove access to RDS for old rackspace server

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,41 +6,6 @@ locals {
   instance_count = 1
 }
 
-# user for rackspace server
-
-resource "aws_iam_user" "rackspace_server" {
-  name = "rackspace-server"
-}
-
-resource "aws_iam_policy" "rackspace_policy" {
-  name        = "rackspace-policy"
-  description = "A policy for access from the Rackspace server"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:DeleteObject",
-        "s3:GetObject",
-        "s3:PutObject",
-        "s3:PutObjectAcl",
-        "s3:ListBucket"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_user_policy_attachment" "rackspace_policy_attach" {
-  user = aws_iam_user.rackspace_server.name
-  policy_arn = aws_iam_policy.rackspace_policy.arn
-}
-
 # fastly logs bucket
 
 resource "aws_s3_bucket" "fastly_logs_bucket" {
@@ -161,7 +126,6 @@ resource "aws_security_group" "allow_postgres" {
     to_port     = 5432
     protocol    = "tcp"
     cidr_blocks = [
-      "23.253.149.7/32", # clojars.org on rackspace
       "172.31.43.101/32" # clojars.org on ec2, private IP. Need to use the value from beta.tf!
     ] 
   }
