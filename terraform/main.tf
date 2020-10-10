8baf38002b215e7c343dc9dc52c1add9f8aaab62
Toby Crawley <toby@tcrawley.org>
2020-10-10 10:10:28 -0400

    Release new AMI

--- terraform/main.tf
+++ terraform/main.tf
@@ -396,7 +396,7 @@ resource "aws_s3_bucket" "deployments_bucket" {
 
 resource "aws_launch_configuration" "prod_launch_config" {
   name_prefix     = "prod-asg-"
-  image_id        = "ami-03ec5dfb28f914406"
+  image_id        = "ami-0acd82b278be0c8e4"
   instance_type   = "t3a.medium"
   key_name        = "server"
   security_groups = [aws_security_group.server_production.id]
