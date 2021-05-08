e44107ce1e0cd04a3c80ffdf5022e1ddc95a35df
Toby Crawley <toby@tcrawley.org>
2021-05-08 10:18:32 -0400

    Update AMI

--- terraform/main.tf
+++ terraform/main.tf
@@ -395,7 +395,7 @@ resource "aws_s3_bucket" "deployments_bucket" {
 
 resource "aws_launch_configuration" "prod_launch_config" {
   name_prefix     = "prod-asg-"
-  image_id        = "ami-098f29ab1f6474b5a"
+  image_id        = "ami-0c0afcbde24ae16df"
   instance_type   = "t4g.medium"
   key_name        = "server"
   security_groups = [aws_security_group.server_production.id]
