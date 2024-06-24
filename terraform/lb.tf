02f51fd94057680ddb4e205e73ba2d8d82547b57
Toby Crawley <toby@tcrawley.org>
2024-06-24 07:10:00 -0400

    Upgrade LB ssl policy
    This drops the deprecated TLS 1.0 & 1.1 protocols, and adds TLS 1.3,
    making the only supported protocols 1.2 & 1.3.
    
    1.2 has been supported by Java since Java 7, and 1.3 since 2020 releases
    of Java 8.

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -82,7 +82,7 @@ resource "aws_lb_listener" "production" {
   port              = "443"
   protocol          = "HTTPS"
 
-  ssl_policy      = "ELBSecurityPolicy-2016-08"
+  ssl_policy      = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn = aws_acm_certificate.lb_tls_cert.arn
 
   default_action {
