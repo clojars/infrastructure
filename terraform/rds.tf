2106f6dc865cfd2079ecfeef1389fb65e011836f
Toby Crawley <toby@tcrawley.org>
2026-05-30 11:42:02 -0400

    Adjust RDS config to match new DB version

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -29,7 +29,7 @@ resource "aws_db_instance" "production" {
   allocated_storage            = 20
   backup_retention_period      = 7
   engine                       = "postgres"
-  engine_version               = "15.12"
+  engine_version               = "18.4"
   identifier                   = "clojars-production2"
   instance_class               = "db.t4g.small"
   db_name                      = "clojars"
